c7408a2ab93ebaa8d0659fbf2ece82e0a121c729
SJoshua <JoshuaSRKF@gmail.com>
2020-06-11 11:02:28 +0800

    feat: update database model

--- t_blog/database.py
+++ t_blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+mysqldb://t_blog:dbpass@localhost/t_blog', convert_unicode=True)
+engine = create_engine('mysql+mysqldb://t_blog:dbpass@localhost/t_blog?charset=utf8mb4', convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
--- t_blog/models.py
+++ t_blog/models.py
@@ -1,64 +1,95 @@
-from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Table, Boolean, Text, ForeignKey
 from .database import Base
 
-class User(Base):
-    __tablename__ = 'users'
+class Article(Base):
+    __tablename__ = 'tb_articles'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
-    password = Column(String(20))
-    describe = Column(String(200),nullable=True)
+    title = Column(Text(collation='utf8mb4_unicode_ci'))
+    date = Column(DateTime)
+    content = Column(Text(collation='utf8mb4_unicode_ci'))
+    author = Column(Integer, ForeignKey('tb_users.id'))
+    category = Column(Integer, ForeignKey('tb_categories.id'))
+    
+    def __init__(self, title=None, content=None, author=None, category=0):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.category = category
+
+    def __repr__(self):
+        return '<Article %r>' % (self.id)
+
+class User(Base):
+    __tablename__ = 'tb_users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(60, collation='utf8mb4_unicode_ci'), unique=True)
+    password = Column(String(255))
+    nickname = Column(String(60, collation='utf8mb4_unicode_ci'))
+    email = Column(String(100), unique=True)
+
+    def __init__(self, username=None, password=None, nickname=None, email=None):
+        self.username = username
+        self.password = password
+        self.nickname = nickname
+        self.email = email
 
     def __repr__(self):
         return '<User %r>' % (self.name)
 
-class Tag(Base):
-    __tablename__ = 'tag'
-    name = Column(String(50), primary_key=True)
-    describe = Column(String(200),nullnullable=True)
-    
-    def __repr__(self):
-        return '<Tag %r>' % (self.name)
-
 class Category(Base):
-    __tablename__ = 'category'
-    name = Column(String(50), primary_key=True)
+    __tablename__ = 'tb_categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50, collation='utf8mb4_unicode_ci'))
     
+    def __init__(self, name=None):
+        self.name = name
+
     def __repr__(self):
         return '<Category %r>' % (self.name)
 
-class Blog(Base):
-    __tablename__ = 'blog'
+class Tag(Base):
+    __tablename__ = 'tb_tags'
     id = Column(Integer, primary_key=True)
-    title = Column(String(50))
-    date = Column(String(50))
-    content = Column(String(collation='utf8'))
-    author_id= Column(String(50),ForeignKey('users.name'))
-    Category_name=Column(String(50),ForeignKey('category.name'))
+    name = Column(String(50, collation='utf8mb4_unicode_ci'))
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return '<Tag %r>' % (self.name)
+
+class Tag_Relation(Base):
+    __tablename__ = 'tb_tag_relations'
+    id = Column(Integer, primary_key=True)
+    article_id = Column(Integer, ForeignKey('tb_articles.id'))
+    tag_id = Column(Integer, ForeignKey('tb_tags.id'))
+    
+    def __init__(self, article_id=None, tag_id=None):
+        self.article_id = article_id
+        self.tag_id = tag_id
     
     def __repr__(self):
-        return '<Blog %r>' % (self.id)
-
-class Set_tag(Base):
-    __tablename__ = 'set_tag'
-    tag_name=Column(String(50),ForeignKey('tag.name'),primary_key=True)
-    blog_id =Column(Integer,ForeignKey('blog.id'),primary_key=True)
-        
-    def __repr__(self):
-        return '<Set_tag %r>' % (self.id)
+        return '<Tag_Relation %r:%r>' % (article_id, tag_id)        
 
 class Comment(Base):
-    __tablename__ = 'comment'
+    __tablename__ = 'tb_conments'
     id = Column(Integer, primary_key=True)
-    blog_id =Column(Integer,ForeignKey('blog.id'))
-    content=content = Column(String(collation='utf8'))
-    date = Column(String(50))
+    article_id = Column(Integer, ForeignKey('tb_articles.id'))
+    author = Column(String(50, collation='utf8mb4_unicode_ci'))
+    email = Column(String(50))
+    content = Column(Text(collation='utf8mb4_unicode_ci'))
+    date = Column(DateTime)
     ip = Column(String(50))
-    author_nickname = Column(String(50))
-    father_comment =Column(Integer)   
+    approved = Column(Boolean)
     
+    def __init__(self, article_id=None, author=None, email=None, content=None, ip=None, approved=True):
+        self.article_id = article_id
+        self.author = author
+        self.email = email
+        self.content = content
+        self.ip = ip
+        self.approved = approved
+
     def __repr__(self):
         return '<Comment %r>' % (self.id)
-
-
 
